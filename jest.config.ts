c181a399dda333923255eea1b7e6b27981ccb985
thomasthiebaud <thiebaud.tom@gmail.com>
2022-05-06 09:48:26 +0200

    fix: update dependencies to fix vulnerabilities

--- jest.config.ts
+++ jest.config.ts
@@ -3,7 +3,6 @@ import type { Config } from "@jest/types";
 const config: Config.InitialOptions = {
   preset: "ts-jest",
   testEnvironment: "node",
-  forceExit: true,
 };
 
 export default config;
